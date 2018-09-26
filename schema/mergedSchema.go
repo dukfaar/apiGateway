2c15108fa17430b30e6a20b3dbb259f471a8d3a1
Robert Schumann <robert.schumann@unic.com>
2018-09-26 11:00:48 +0200

    refactoring

--- schema/mergedSchema.go
+++ schema/mergedSchema.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"reflect"
 	"strings"
-	"time"
 
 	"github.com/gorilla/websocket"
 
@@ -24,66 +23,6 @@ type MergedSchemas struct {
 	inputTypes        map[string]*graphql.InputObject
 	typeExtensions    map[string]map[string]bool
 	serviceInfoByType map[string]eventbus.ServiceInfo
-}
-
-func serializeDate(value interface{}) interface{} {
-	switch value := value.(type) {
-	case time.Time:
-		buff, err := value.MarshalText()
-		if err != nil {
-			return nil
-		}
-
-		return string(buff)
-	case *time.Time:
-		return serializeDate(*value)
-	case string:
-		return value
-	case *string:
-		return *value
-	default:
-		return nil
-	}
-}
-
-func deserializeDate(value interface{}) interface{} {
-	fmt.Println(value)
-	return nil
-}
-
-var dataScalar *graphql.Scalar = graphql.NewScalar(graphql.ScalarConfig{
-	Name:        "Date",
-	Description: "The `Date` scalar type represents a Date.",
-	Serialize:   serializeDate,
-	ParseValue:  deserializeDate,
-	ParseLiteral: func(valueAST ast.Value) interface{} {
-		switch valueAST := valueAST.(type) {
-		case *ast.StringValue:
-			return valueAST.Value
-		}
-		return nil
-	},
-})
-
-func getScalarTypeDefinition(fieldType *FieldType) graphql.Output {
-	switch *fieldType.Name {
-	case "String":
-		return graphql.String
-	case "ID":
-		return graphql.ID
-	case "Boolean":
-		return graphql.Boolean
-	case "Float":
-		return graphql.Float
-	case "Int":
-		return graphql.Int
-	case "DateTime":
-		return graphql.DateTime
-	case "Date":
-		return dataScalar
-	default:
-		panic("Unknown TypeName " + *fieldType.Name)
-	}
 }
 
 func (m *MergedSchemas) getTypeDefinition(fieldType *FieldType) graphql.Output {
